adc813078b8691edc208ef9d2db75920a6a77370
EwertonLeal <ewertonolivaleal6@gmail.com>
2024-11-13 18:25:09 -0300

    Limpando a pasta layout e removendo elementos que não irei utilizar em vertical.component e topbar.component, alterando e removendo estilos que não usarei

--- src/app/layouts/layouts.module.ts
+++ src/app/layouts/layouts.module.ts
@@ -7,19 +7,15 @@ import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
 
 import { UIModule } from '../shared/ui/ui.module';
 import { LayoutComponent } from './layout.component';
-import { SidebarComponent } from './sidebar/sidebar.component';
 import { TopbarComponent } from './topbar/topbar.component';
 import { FooterComponent } from './footer/footer.component';
-import { RightsidebarComponent } from './rightsidebar/rightsidebar.component';
-import { HorizontalComponent } from './horizontal/horizontal.component';
 import { VerticalComponent } from './vertical/vertical.component';
-import { HorizontaltopbarComponent } from './horizontaltopbar/horizontaltopbar.component';
 import { LanguageService } from '../core/services/language.service';
 import { TranslateModule } from '@ngx-translate/core';
 
 @NgModule({
   // tslint:disable-next-line: max-line-length
-  declarations: [LayoutComponent, SidebarComponent, TopbarComponent, FooterComponent, RightsidebarComponent, HorizontalComponent, VerticalComponent, HorizontaltopbarComponent],
+  declarations: [LayoutComponent, TopbarComponent, FooterComponent, VerticalComponent],
   imports: [
     CommonModule,
     TranslateModule,
